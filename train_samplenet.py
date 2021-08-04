b05c3a7a3dc62fcba83c75db8cfc77cea176325c
ygjken <ken.748322@gmail.com>
2021-08-05 00:00:43 +0900

    :sparkles:add Validation loss to train script, :boob:but loss is bad

--- train_samplenet.py
+++ train_samplenet.py
@@ -6,20 +6,33 @@ from utils.dataloader import ModelNetCls
 from torch.utils import data
 from torch import optim
 import torch
+from torch.utils.tensorboard import SummaryWriter
 
 SAMPLED_SIZE = 256
 
+writer = SummaryWriter("log/down_and_up_sample")
+
 # data
-modelnet40 = ModelNetCls(
+modelnet40_train = ModelNetCls(
     num_points=1024,
     transforms=None,
     train=True
 )
 
-train_loader = data.DataLoader(modelnet40, batch_size=32, shuffle=True)
+
+modelnet40_vali = ModelNetCls(
+    num_points=1024,
+    transforms=None,
+    train=False
+)
+
+data_loaders = {'train':
+                data.DataLoader(modelnet40_train, batch_size=32, shuffle=True),
+                'val':
+                data.DataLoader(modelnet40_vali, batch_size=16, shuffle=True)}
 
 # models
-encoder = SampleNet(
+downsampler = SampleNet(
     num_out_points=SAMPLED_SIZE,
     bottleneck_size=128,
     group_size=10,
@@ -29,21 +42,20 @@ encoder = SampleNet(
     skip_projection=True
 )
 
-decoder = SampleNetDecoder(
+upsampler = SampleNetDecoder(
     num_sampled_points=SAMPLED_SIZE,
     bottleneck_size=128,
     num_reconstracted_points=1024,
 )
 
-encoder.cuda()
-encoder.train()
-decoder.cuda()
-decoder.train()
+downsampler.cuda()
+upsampler.cuda()
+
 
 # optimiter
-encoder_params = filter(lambda p: p.requires_grad, encoder.parameters())
+encoder_params = filter(lambda p: p.requires_grad, downsampler.parameters())
 encoder_optimizer = optim.Adam(encoder_params, lr=1e-3)
-decoder_params = filter(lambda p: p.requires_grad, decoder.parameters())
+decoder_params = filter(lambda p: p.requires_grad, upsampler.parameters())
 decoder_optimizer = optim.Adam(decoder_params, lr=1e-3)
 
 
@@ -53,32 +65,48 @@ lmbda = 0.01
 
 
 for epoch in range(300):
-    for pc in train_loader:
-        pc_pl = pc[0].clone().detach().cuda()
 
-        # train
-        simp_pc, proj_pc, _ = encoder(pc_pl)
-        pred = decoder(proj_pc)
+    for phase in ['train', 'val']:
+        # Select mode
+        if phase == 'train':
+            downsampler.train()
+            upsampler.train()
+        else:
+            downsampler.eval()
+            upsampler.eval()
 
-        # Compute losses
-        simplification_loss = encoder.get_simplification_loss(
-            pc_pl, simp_pc, 64
-        )
-        projection_loss = encoder.get_projection_loss()
-        reconstrution_loss = decoder.get_reconstruction_loss(pc_pl, pred)
+        for pc in data_loaders[phase]:
+            pc_pl = pc[0].clone().detach().cuda()
 
-        loss = reconstrution_loss + \
-            alpha * simplification_loss + lmbda * projection_loss
+            with torch.set_grad_enabled(phase == "train"):
+                # Forward Propagation
+                simp_pc, proj_pc, _ = downsampler(pc_pl)
+                pred = upsampler(proj_pc)
 
-        # Backward + Optimize
-        encoder_optimizer.zero_grad()
-        decoder_optimizer.zero_grad()
-        loss.backward()
-        encoder_optimizer.step()
-        decoder_optimizer.step()
+                # Compute losses
+                simplification_loss = downsampler.get_simplification_loss(
+                    pc_pl, simp_pc, SAMPLED_SIZE
+                )
+                projection_loss = downsampler.get_projection_loss()
+                reconstrution_loss = upsampler.get_reconstruction_loss(
+                    pc_pl, pred)
 
-    print(f"epoch: {epoch}, {loss}", flush=True)
-    torch.save(encoder.state_dict(), "weight/encoder.pt")
-    torch.save(decoder.state_dict(), "weight/decoder.pt")
+                loss = reconstrution_loss + \
+                    alpha * simplification_loss + lmbda * projection_loss
 
+            if phase == 'train':
+                # Backward + Optimize
+                encoder_optimizer.zero_grad()
+                decoder_optimizer.zero_grad()
+                loss.backward()
+                encoder_optimizer.step()
+                decoder_optimizer.step()
+
+        print(f"{epoch}, {phase}, {loss}", flush=True)
+        writer.add_scalar(f'{phase}', loss, epoch)
+        torch.save(downsampler.state_dict(), "weight/encoder.pt")
+        torch.save(upsampler.state_dict(), "weight/decoder.pt")
+
+writer.flush()
+writer.close()
 print("done!")
